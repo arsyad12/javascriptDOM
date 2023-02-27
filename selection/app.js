f38c6f770aebbf6b1ac5318ff17ccb8c270d8458
asyad <asyadbaso123@gmail.com>
2023-02-27 15:08:04 +0700

    ini

---
+++ selection/app.js
@@ -0,0 +1,28 @@
+
+//Contoh penggunaan document.getElementsByTagName
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+// 	// console.log(img.src);
+
+/// jika source gmbar sudah ditentukan menggunakan ketentuan dibawah ini, maka img tidak akan daimbil dari katalog image dari fungsi allImages
+
+// 	img.src =
+// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
+// }
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for (let img of squareImages) {
+// 	img.src =
+// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
+// }
+
+const links = document.querySelectorAll('a');
+
+for (let link of links) {
+	link.style.color = 'rgb(0, 108, 134)';
+	link.style.textDecorationColor = 'magenta';
+	link.style.textDecorationStyle = 'wavy';
+}
